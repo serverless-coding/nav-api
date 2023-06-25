842b019be3ddc295f0747cdbfde38bca5de822e4
yiGmMk <marvelousme@163.com>
2023-06-25 07:39:25 +0000

    refactor: api change.3

--- app/links.ts
+++ app/links.ts
@@ -27,6 +27,11 @@ export default async function getNavLinks(): Promise<CategoryWithLinks[]> {
   const res = await fetch("https://nav.programnotes.cn/api/link");
   const data: Data = await res.json();
   console.log("fetch category res:", data);
+  data.data.map((c) => {
+    if (c.links == undefined) {
+      c.links=[];
+    }
+  })
   return data.data;
 }
 
