a617e597c2295a710c82fbf75ab496f37f295e15
yi_t <marvelousme@163.com>
2023-07-04 13:38:38 +0800

    feat: navi-api link

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,9 @@ export default function Home() {
       <ul className='flex relative ml-0'>
         <li className='font-bold mr-4'><strong>Nav-Api</strong></li>
         <li className='font-bold mr-4'><Link href='https://nav.programnotes.cn'>Nav</Link></li>
-        <li className='font-bold'><Link href='https://github.com/serverless-coding/frontend-nav'>Github</Link></li>
+        <li className='font-bold'>
+          <Link href='https://github.com/serverless-coding/frontend-nav'>Github</Link>
+        </li>
       </ul>
       <div className="relative flex place-items-center items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
@@ -25,7 +27,11 @@ export default function Home() {
           priority
         />
       </div>
-      <footer className='relative flex justify-start'><p>&copy; serverless</p></footer>
+      <footer className='relative flex justify-start'>
+        <Link href='https://github.com/serverless-coding/nav-api'>
+          <p>&copy; serverless</p>
+        </Link>
+      </footer>
     </main >
   )
 }
